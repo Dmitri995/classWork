10a199b5459491a21f655b0c3d174a0a53651459
Dmitri995 <dm.khlystov995@gmail.com>
2023-12-22 16:53:49 +0500

    lesson_3 meal done

--- lesson_3/food/app_food/urls.py
+++ lesson_3/food/app_food/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import main, catalog, category
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from .views import main_page, login_view, logout_view, MyLogoutView
 
 urlpatterns = [
     path('catalog/', catalog, name='catalog'),
     path('', main, name='main'),
-    path('catalog/category/<int:pk>/', category, name='category')
+    path('catalog/category/<int:pk>/', category, name='category'),
+    path('login/', LoginView.as_view(template_name='app_food/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout', MyLogoutView.as_view(), name='logout')
 ]
+
--- lesson_3/food/app_food/views.py
+++ lesson_3/food/app_food/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from .models import Product, Category
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
 
 
 def main(request, *args, **kwargs):
@@ -17,3 +20,40 @@ def category(request, *args, **kwargs):
     return render(request, 'app_food/category.html', context={
         'products': products
     })
+
+
+
+
+
+
+
+
+
+
+
+
+def main_page(request, *args, **kwargs):
+    return render(request, 'app_food/base.html')
+
+
+def login_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return render(request, 'app_food/login.html')
+
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('main')
+
+    return render(request, 'app_food/login.html', {'error': 'auth error'})
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy('main')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('main')
